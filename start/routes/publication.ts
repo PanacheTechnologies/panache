e897f1c1dab02b8837ec85e4f95590cffe9b2ea9
Alexis Bouchez <alexbcz@proton.me>
2025-04-05 21:54:08 +0200

    fix routing

--- start/routes/publication.ts
+++ start/routes/publication.ts
@@ -1,6 +1,6 @@
 import router from '@adonisjs/core/services/router'
 
-const PublicationController = () => import('#controllers/publication_controller')
+const PublicationController = () => import('#controllers/publication-controller')
 
 router.get('/p/:slug', [PublicationController, 'listPosts']).as('listPosts')
 router.get('/p/:slug/posts/:id', [PublicationController, 'showPost'])

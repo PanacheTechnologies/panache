b893171b7674d6ea22df94c8fffd89c2d7a4a36b
Alexis Bouchez <alexbcz@proton.me>
2025-04-06 10:35:43 +0200

    add legal pages

--- inertia/components/documentation/documentation-layout.tsx
+++ inertia/components/documentation/documentation-layout.tsx
@@ -1,6 +1,9 @@
 import type { PropsWithChildren } from 'react'
 import { MarketingLayout } from '../marketing-layout/marketing-layout'
 
-export function DocumentationLayout({ children }: PropsWithChildren) {
-  return <MarketingLayout showBottomBorder>{children}</MarketingLayout>
+export function DocumentationLayout({
+  children,
+  className,
+}: PropsWithChildren & { className?: string }) {
+  return <MarketingLayout className={className}>{children}</MarketingLayout>
 }
--- inertia/components/marketing-layout/marketing-layout.tsx
+++ inertia/components/marketing-layout/marketing-layout.tsx
@@ -1,12 +1,20 @@
 import { Footer } from './footer'
 import { Header, type HeaderProps } from './header/header'
 import type { PropsWithChildren } from 'react'
+import { cn } from '~/lib/utils'
+export type MarketingLayoutProps = {
+  className?: string
+} & PropsWithChildren &
+  HeaderProps
 
-export type MarketingLayoutProps = {} & PropsWithChildren & HeaderProps
-
-export function MarketingLayout({ children, ...headerProps }: MarketingLayoutProps) {
+export function MarketingLayout({ children, className, ...headerProps }: MarketingLayoutProps) {
   return (
-    <div className="flex flex-col justify-between min-h-screen space-y-4 w-full max-w-5xl mx-auto p-4">
+    <div
+      className={cn(
+        'flex flex-col justify-between min-h-screen space-y-4 w-full max-w-5xl mx-auto p-4',
+        className
+      )}
+    >
       <Header {...headerProps} />
       <main>{children}</main>
       <Footer />
